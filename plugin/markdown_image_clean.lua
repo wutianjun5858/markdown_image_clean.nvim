4536f786fecaa21d4ff2799be59bd4445d15197e
Jonathan.Wu <tianjun.wu@vastaitech.com>
2025-08-06 15:42:36 +0800

    feat: add clean dir

--- plugin/markdown_image_clean.lua
+++ plugin/markdown_image_clean.lua
@@ -8,7 +8,8 @@ vim.g.loaded_markdown_image_clean = 1
 
 -- 创建用户命令
 vim.api.nvim_create_user_command('MarkdownImageClean', function(opts)
-  require('markdown_image_clean').clean_images(opts.args)
+  local dir = opts.args and opts.args ~= "" and opts.args or nil
+  require('markdown_image_clean').clean_images(dir)
 end, {
   nargs = '?',
   desc = '清理 Markdown 文件中未引用的图片',
